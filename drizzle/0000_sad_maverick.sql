4256480db0b0590c5f7d95ba8d0b886943a5d0dd
Stephen Park <ssp0929@gmail.com>
2025-03-08 21:51:21 -0800

    chore: remove files

--- drizzle/0000_sad_maverick.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-	`id` integer PRIMARY KEY NOT NULL,
-	`username` text NOT NULL,
-	`password` text NOT NULL,
-	`created_at` text DEFAULT 'CURRENT_TIMESTAMP' NOT NULL,
-	`updated_at` text DEFAULT 'CURRENT_TIMESTAMP' NOT NULL
-);
